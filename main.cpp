86ef5f4455d403386544bd37d76ef812f14a0110
ayoub.agouzoul <ayoub.agouzoul@polytechnique.edu>
2024-12-05 12:08:27 +0100

    add back my files

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include "Board.h"
+#include "Game.h"
 #include "Square.h"
 #include "pieces/Piece.h"
 #include <iostream>
 #include <string>
+#include <unordered_map>
 
 int main(int argc, char **argv) {
   Board b;
@@ -11,5 +13,60 @@ int main(int argc, char **argv) {
   Piece *wk = b.board[0][4].piece;
   Pos p{0, 6};
   std::cout << wk->potential_move(p) << std::endl;
+
+  for(int i = 0; i < argc; i++) {
+    std::cout << "Displaying argument #" << std::to_string(i) << ": `"
+              << std::string(argv[i]) << "'" << std::endl;
+  }
+
+  std::unordered_map<std::string, std::string> argMap;
+
+  for(int i = 1; i < argc; i++) {
+    std::string arg = argv[i];
+    if(arg == "-H" || arg == "-m") {
+      if(i + 1 < argc) {
+        argMap[arg] = argv[++i]; // store next argument
+      } else {
+        std::cerr << "Error: Missing value for option `" << arg << "`."
+                  << std::endl;
+        return 1;
+      }
+    } else {
+      std::cerr << "Error: Unrecognized argument `" << arg << "`." << std::endl;
+      return 1;
+    }
+  }
+  // <yourAIname> -H <input history file> -m <output move file>
+
+  // FIXME: -m -H zdzdz considers -H as arg to -m !!!
+
+  if(argMap.count("-H")) {
+    std::cout << "Input history filename: " << argMap["-H"] << std::endl;
+  } else {
+    std::cerr << "Error: -H option is required." << std::endl;
+    return 1;
+  }
+
+  if(argMap.count("-m")) {
+    std::cout << "Output move file: " << argMap["-m"] << std::endl;
+  } else {
+    std::cerr << "Error: -m option is required." << std::endl;
+    return 1;
+  }
+
   return 0;
 }
+
+/*
+How to open a file for reading :
+std::ifstream inputFile("input.txt");
+
+How to open a file for writing :
+std::ofstream outputFile("output.txt");
+
+Read a line from the file :
+std::getline(inputFile, line)
+--> while (std::getline(inputFile, line))
+it reads each line from the file and stores it in the line string, it continues
+until the end of the file is reached
+*/
